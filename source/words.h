916cfbfe80f5db0f1abf97370be0a4333828ef2c
Tamerlan Beyimjan <beyimjan@gmail.com>
2024-01-05 00:45:38 +0000

    Update comment in words.h

--- source/words.h
+++ source/words.h
@@ -10,7 +10,7 @@ struct words_t {
 struct words_t init_words();
 void add_word(struct words_t *words, char *word);
 
-/* You should not use words after calling `free_words` */
+/* You should not use `words` after calling `free_words` */
 void free_words(struct words_t *words);
 
 #endif
